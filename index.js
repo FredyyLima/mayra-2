63554741bfc67f1ff13d64384a26e1938697aef8
Frederico Lima <fredyy_lima@live.com>
2023-06-21 12:34:54 -0300

    .

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const path = require('path');
 app.use(express.static(path.join(__dirname, 'assets')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'assets', 'index.html'));
+  res.sendFile(path.join(__dirname,'index.html'));
 });
 
 const port = process.env.PORT || 3000;
